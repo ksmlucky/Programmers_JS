3ca7b33e56e9d0be5bce0b8f627f399540c51e10
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-10-05 23:56:25 +0900

    멀리뛰기

---
+++ LongJump.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var dp = [0, 1, 2];
+    for (var i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+
+    return dp[n];
+}
+
+console.log(solution(4));
+console.log(solution(3));
